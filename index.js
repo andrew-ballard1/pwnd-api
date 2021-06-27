8f8db6fec8fe0c397b9dbd33f228247f0226b038
andrew-ballard1 <andrewj.ballard94@gmail.com>
2021-06-27 09:58:19 -0700

    Releasing port variable for heorku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = 3030
+const port = process.env.PORT || 3030
 const fetch = require('node-fetch')
 
 app.use(cors())
